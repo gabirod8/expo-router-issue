8fb2682b10a0f1b59c163fe044856925bedb8ea4
gabirod8 <gabirod8@gmail.com>
2025-02-18 18:57:44 -0300

    Update _layout.tsx

--- my-app/app/home/_layout.tsx
+++ my-app/app/home/_layout.tsx
@@ -15,7 +15,7 @@ export default function StackLayout() {
           })}
         />
         <Stack.Screen
-          name="detail"
+          name="details"
           options={() => ({
             title: 'Details',
           })}
